bd0200d2e0190b476f0cda0443115206bcacbc8f
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-03-20 11:13:18 -0300

    Iconos y ruta activa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default function HomePage() {
 
-  redirect('/dashboard/counter')
+  redirect('/dashboard/main')
 
 }
