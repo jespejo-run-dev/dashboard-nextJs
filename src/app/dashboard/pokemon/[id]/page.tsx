abc985515a1e55b60246c37ca5438c63f79eb5b7
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-03-24 15:16:16 -0300

    Not found Page - 404

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { Pokemon } from "@/pokemons/interfaces/pokemon";
 import { Metadata } from "next";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 
 interface Props {
@@ -11,23 +12,39 @@ interface Props {
 
 export async function generateMetadata({params}: Props): Promise<Metadata>{
 
+  try {
     const {id, name} = await getPokemon(params.id);
 
     return {
         title: `${id} - ${name}`,
         description: `Página del pokemon ${name}`
     }
+  } catch (error) {
+    return {
+      title: 'Pagina del pokemon',
+      description: 'Página del pokemon no encontrado'
+    }
+  }
+
 }
 
 
 const getPokemon = async (id: string): Promise<Pokemon> => {
-    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
-        cache: 'force-cache', // TODO cambiar esto en un futuro
-        // next: {
-        //     revalidate: 60*60*30*6
-        // }
-    }).then(resp => resp.json());
-    return pokemon;
+
+    try {
+
+      const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+          cache: 'force-cache', // TODO cambiar esto en un futuro
+          // next: {
+          //     revalidate: 60*60*30*6
+          // }
+      }).then(resp => resp.json());
+      return pokemon;
+
+    } catch (error) {
+      notFound();
+    }
+
 }
 
 
