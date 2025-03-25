6fa4c77cba9bc3f7f2e4da0a5dda51b65fe300aa
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-03-25 15:17:18 -0300

    Counter Reducer y actions

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/store";
+import { addOne, substractOne } from "@/store/counter/counterSlice";
 
 interface Pops {
     value: number
@@ -8,17 +9,22 @@ interface Pops {
 
 export const CartCounter = ({value = 0}: Pops) => {
 
-    const [counter, setCounter] = useState(value);
+    // const [counter, setCounter] = useState(value);
+
+    const count = useAppSelector(state => state.counter.count);
+    const dispatch = useAppDispatch();
 
   return (
     <>
-    <span className="text-9xl font-bold">{counter}</span>
+    <span className="text-9xl font-bold">{count}</span>
         <div className="flex">
-        <button onClick={() => setCounter(counter + 1)} 
+        <button 
+                onClick={() => dispatch(addOne())} 
                 className="flex items-center justify-center rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2 p-2 cursor-pointer">
             +1
         </button>
-        <button onClick={() => setCounter(counter - 1)} 
+        <button 
+                onClick={() => dispatch(substractOne())} 
                 className="flex items-center justify-center rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2 p-2 cursor-pointer">
             -1
         </button>
--- src/store/counter/counterSlice.ts
+++ src/store/counter/counterSlice.ts
@@ -1,21 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 interface CounterState {
-    counter: number;
+  count: number;
 }
 
 const initialState: CounterState = {
-    counter: 5
+  count: 5
 }
 
 const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    
+    addOne(state) {
+      state.count++
+    },
+    substractOne(state) {
+      if (state.count === 0) return
+      state.count--
+    },
+    resetCount(state, action: PayloadAction<number>) {
+      if (action.payload < 0) action.payload = 0
+
+      state.count = action.payload
+    }    
   }
 });
 
-export const {} = counterSlice.actions
+export const { addOne, substractOne, resetCount} = counterSlice.actions
 
 export default counterSlice.reducer
